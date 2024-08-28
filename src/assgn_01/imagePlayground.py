c917603e2ca54295650dba10a1774b74dae06830
Thada Srisaluaikun <gnar875@gmail.com>
2024-08-28 22:53:14 +0700

    zoom function

--- src/assgn_01/imagePlayground.py
+++ src/assgn_01/imagePlayground.py
@@ -2,12 +2,41 @@ import cv2 as cv
 import numpy as np
 
 from cv2.typing import MatLike
+from numpy.typing import NDArray
+
+
+def readImageGray(filename: str) -> MatLike:
+    return cv.imread(filename=filename, flags=cv.IMREAD_GRAYSCALE)
 
 
 def showImage(image: MatLike) -> None:
-    cv.imshow(winname="", mat=image)
+    cv.imshow(winname="Thada", mat=image)
     cv.waitKey(delay=0)
     cv.destroyAllWindows()
+
+
+def replicationZoom(zoomFactor: float, image: MatLike) -> MatLike:
+    newWidth: int = int(image.shape[1] * zoomFactor)
+    newHeight: int = int(image.shape[0] * zoomFactor)
+    newImage: NDArray = np.zeros(shape=(newHeight, newWidth), dtype=image.dtype)
+
+    # TODO: maybe can use map function or any numpy dStruct(ogrid) for better performance
+    # but nested loop is not that bad
+    for m in range(newHeight):
+        for n in range(newWidth):
+            oldM: int = int(m / zoomFactor)
+            oldN: int = int(n / zoomFactor)
+            newImage[m, n] = image[oldM, oldN]
+    return newImage
+
+
+def bilinearZoom(zoomFactor: float, image: MatLike) -> MatLike:
+    newWidth: int = int(image.shape[1] * zoomFactor)
+    newHeight: int = int(image.shape[0] * zoomFactor)
+
+    return cv.resize(
+        src=image, dsize=(newWidth, newHeight), interpolation=cv.INTER_LINEAR
+    )
 
 
 def makeGivenFunction(l: int):
@@ -36,5 +65,6 @@ def makePowerLawTransformation(c: float, gamma: float):
     return powerLawTransformation
 
 
-img: MatLike = cv.imread("flower.jpg")
-showImage(image=img)
+image: MatLike = readImageGray(filename="../../assets/flower.jpg")
+zoomImage: MatLike = replicationZoom(zoomFactor=3, image=image)
+showImage(image=zoomImage)
