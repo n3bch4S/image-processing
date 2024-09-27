563d06be5974b0cd2d4fc3705112123efa874c56
Thada Srisaluaikun <gnar875@gmail.com>
2024-09-27 23:43:15 +0700

    move file

--- src/01_zoomAndMap.py
+++ src/01_zoomAndMap.py
@@ -87,178 +87,178 @@ def makePowerLawTransformation(c: float, gamma: float):
 
 
 fileDirectory: list[str] = [
-    "../../assets/flower.jpg"  # index 0
-    "../../assets/fractal.jpg"  # index 1
-    "../../assets/galaxy3.jpeg"  # index 2
-    "../../assets/spellbound_mini.jpg"  # index 3
-    "../../assets/superpets_mini.jpg"  # index 4
-    "../../assets/traffic.jpg"  # index 5
+    "../assets/assgn_01/flower.jpg"  # index 0
+    "../assets/assgn_01/fractal.jpg"  # index 1
+    "../assets/assgn_01/galaxy3.jpeg"  # index 2
+    "../assets/assgn_01/spellbound_mini.jpg"  # index 3
+    "../assets/assgn_01/superpets_mini.jpg"  # index 4
+    "../assets/assgn_01/traffic.jpg"  # index 5
 ]
 # TODO: can turn all case to dict...
 # replicate x3 flower
-image: MatLike = readImageGray(filename="../../assets/flower.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/flower.jpg")
 zoomImage: MatLike = replicationZoom(zoomFactor=3, image=image)
 filename: str = "replicateX3Flower"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # replicate x3 fractal
-image: MatLike = readImageGray(filename="../../assets/fractal.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/fractal.jpg")
 zoomImage: MatLike = replicationZoom(zoomFactor=3, image=image)
 filename: str = "replicateX3Fractal"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # replicate x3 traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 zoomImage: MatLike = replicationZoom(zoomFactor=3, image=image)
 filename: str = "replicateX3Traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # replicate x1/3 flower
-image: MatLike = readImageGray(filename="../../assets/flower.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/flower.jpg")
 zoomImage: MatLike = replicationZoom(zoomFactor=1 / 3, image=image)
 filename: str = "replicateX1_3Flower"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # replicate x1/3 fractal
-image: MatLike = readImageGray(filename="../../assets/fractal.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/fractal.jpg")
 zoomImage: MatLike = replicationZoom(zoomFactor=1 / 3, image=image)
 filename: str = "replicateX1_3Fractal"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # replicate x1/3 traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 zoomImage: MatLike = replicationZoom(zoomFactor=1 / 3, image=image)
 filename: str = "replicateX1_3Traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # bilinear x3 flower
-image: MatLike = readImageGray(filename="../../assets/flower.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/flower.jpg")
 zoomImage: MatLike = bilinearZoom(zoomFactor=3, image=image)
 filename: str = "bilinearX3Flower"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # bilinear x3 fractal
-image: MatLike = readImageGray(filename="../../assets/fractal.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/fractal.jpg")
 zoomImage: MatLike = bilinearZoom(zoomFactor=3, image=image)
 filename: str = "bilinearX3Fractal"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # bilinear x3 traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 zoomImage: MatLike = bilinearZoom(zoomFactor=3, image=image)
 filename: str = "bilinearX3Traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # bilinear x1/3 flower
-image: MatLike = readImageGray(filename="../../assets/flower.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/flower.jpg")
 zoomImage: MatLike = bilinearZoom(zoomFactor=1 / 3, image=image)
 filename: str = "bilinearX1_3Flower"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # bilinear x1/3 fractal
-image: MatLike = readImageGray(filename="../../assets/fractal.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/fractal.jpg")
 zoomImage: MatLike = bilinearZoom(zoomFactor=1 / 3, image=image)
 filename: str = "bilinearX1_3Fractal"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 # bilinear x1/3 traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 zoomImage: MatLike = bilinearZoom(zoomFactor=1 / 3, image=image)
 filename: str = "bilinearX1_3Traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=zoomImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=zoomImage)
 
 
 givenFunction: Callable[[np.uint8], np.uint8] = makeGivenFunction(256)
 # enhance grayscale with givenFunction superpets_mini
-image: MatLike = readImageGray(filename="../../assets/superpets_mini.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/superpets_mini.jpg")
 enhanceImage: MatLike = enhanceGrayScale(image=image, function=givenFunction)
 filename: str = "givenFunction_superpets_mini"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # enhance grayscale with givenFunction traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 enhanceImage: MatLike = enhanceGrayScale(image=image, function=givenFunction)
 filename: str = "givenFunction_traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # enhance grayscale with givenFunction spellbound_mini
-image: MatLike = readImageGray(filename="../../assets/spellbound_mini.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/spellbound_mini.jpg")
 enhanceImage: MatLike = enhanceGrayScale(image=image, function=givenFunction)
 filename: str = "givenFunction_spellbound_mini"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 powerLaw = makePowerLawTransformation(c=0.5, gamma=0.4)
 # powerLaw 0_5 0_4 superpets_mini
-image: MatLike = readImageGray(filename="../../assets/superpets_mini.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/superpets_mini.jpg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 0_5 0_4 superpets_mini"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # powerLaw 0_5 0_4 galaxy3
-image: MatLike = readImageGray(filename="../../assets/galaxy3.jpeg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/galaxy3.jpeg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 0_5 0_4 galaxy3"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # powerLaw 0_5 0_4 traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 0_5 0_4 traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 powerLaw = makePowerLawTransformation(c=0.5, gamma=2.5)
 # powerLaw 0_5 2_5 superpets_mini
-image: MatLike = readImageGray(filename="../../assets/superpets_mini.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/superpets_mini.jpg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 0_5 2_5 superpets_mini"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # powerLaw 0_5 2_5 galaxy3
-image: MatLike = readImageGray(filename="../../assets/galaxy3.jpeg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/galaxy3.jpeg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 0_5 2_5 galaxy3"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # powerLaw 0_5 2_5 traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 0_5 2_5 traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 powerLaw = makePowerLawTransformation(c=1.2, gamma=0.4)
 # powerLaw 1_2 0_4 superpets_mini
-image: MatLike = readImageGray(filename="../../assets/superpets_mini.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/superpets_mini.jpg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 1_2 0_4 superpets_mini"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # powerLaw 1_2 0_4 galaxy3
-image: MatLike = readImageGray(filename="../../assets/galaxy3.jpeg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/galaxy3.jpeg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 1_2 0_4 galaxy3"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # powerLaw 1_2 0_4 traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 1_2 0_4 traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 powerLaw = makePowerLawTransformation(c=1.2, gamma=2.5)
 # powerLaw 1_2 2_5 superpets_mini
-image: MatLike = readImageGray(filename="../../assets/superpets_mini.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/superpets_mini.jpg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 1_2 2_5 superpets_mini"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # powerLaw 1_2 2_5 galaxy3
-image: MatLike = readImageGray(filename="../../assets/galaxy3.jpeg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/galaxy3.jpeg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 1_2 2_5 galaxy3"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
 
 # powerLaw 1_2 2_5 traffic
-image: MatLike = readImageGray(filename="../../assets/traffic.jpg")
+image: MatLike = readImageGray(filename="../assets/assgn_01/traffic.jpg")
 enhanceImage: MatLike = powerLaw(image=image)
 filename: str = "powerLaw 1_2 2_5 traffic"
-cv.imwrite(filename=f"./result/{filename}.jpg", img=enhanceImage)
+cv.imwrite(filename=f"../result/assgn_01/{filename}.jpg", img=enhanceImage)
