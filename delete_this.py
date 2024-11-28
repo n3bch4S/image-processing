19b7b91c8ecb4785bc886f6c824bea5714936938
Thada Srisaluaikun <gnar875@gmail.com>
2024-11-29 03:37:00 +0700

    Update delete_this.py

--- delete_this.py
+++ delete_this.py
@@ -84,12 +84,82 @@ def hsi_to_rgb(h, s, i):
         print(f"(R, G, B) = (I, P, Q) = ({r:.4f}, {g:.4f}, {b:.4f})")
     print("#############\n")
 
-def compliment_of_rgb(r,g,b):
-    
+
+def compliment_of_rgb(r, g, b):
+    print(f"(R, G, B) = ({r:.4f}, {g:.4f}, {b:.4f})")
+    R, G, B = 1 - r, 1 - g, 1 - b
+    print(
+        f"Compliment of (R, G, B) = (1-{r:.4f}, 1-{g:.4f}, 1-{b:.4f}) = ({R:.4f}, {G:.4f}, {B:.4f})"
+    )
+
+
+def show_unique(ndarray: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    unique_values, counts = np.unique(ndarray, return_counts=True)
+
+    # Display results
+    print("Unique Values:", unique_values)
+    print("Counts:", counts)
+    # print("#############\n")
+    return unique_values, counts
+
+
+def split_threshold(
+    matrix: np.ndarray, threshold: int
+) -> tuple[np.ndarray, np.ndarray]:
+    background = matrix[matrix < threshold]
+    print(f"background: {background}")
+    foreground = matrix[matrix >= threshold]
+    print(f"foreground: {foreground}")
+    # print("################\n")
+    return background, foreground
+
+
+def otsu_method(matrix: np.ndarray):
+    unique_values, counts = show_unique(matrix)
+    mu_t = np.mean(matrix)
+    print(f"mu_t = average of an image = {mu_t:.4f}\n")
+    best_t = 0
+    max_sigma_square = 0
+
+    for t in range(1, np.max(matrix) + 1):
+        print(f"at T = {t}")
+        background, foreground = split_threshold(matrix, t)
+        mu_0 = np.mean(background)
+        print(f"mu_0 = average of background = {mu_0:.4f}")
+        omega_0 = len(background) / counts.sum()
+        print(f"omega_0 = {len(background)}/{counts.sum()} = {omega_0:.4f}")
+        mu_1 = np.mean(foreground)
+        print(f"mu_1 = average of foreground = {mu_1:.4f}")
+        omega_1 = len(foreground) / counts.sum()
+        print(f"omega_1 = {len(foreground)}/{counts.sum()} = {omega_1:.4f}")
+        sigma_square = omega_0 * (mu_0 - mu_t) ** 2 + omega_1 * (mu_1 - mu_t) ** 2
+        print(
+            f"sigma_square = {omega_0:.4f}({mu_0:.4f}-{mu_t:.4f})^2 + {omega_1:.4f}({mu_1:.4f}-{mu_t:.4f})^2 = {sigma_square:.4f}"
+        )
+        if sigma_square > max_sigma_square:
+            max_sigma_square = sigma_square
+            best_t = t
+
+        print("#############\n")
+
+    print(f"Best T = {best_t}")
+    print(f"at sigma_square = {max_sigma_square:.4f}")
+    print("###############\n")
+
 
 def main():
-    input = [0.5833, 0.6667, 0.6]
-    hsi_to_rgb(*input)
+    input = np.array(
+        [
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1],
+            [1, 1, 1, 2, 2, 3],
+            [3, 3, 3, 3, 3, 4],
+            [4, 4, 4, 4, 4, 4],
+            [4, 4, 5, 5, 5, 5],
+        ]
+    )
+    # input = [0.2,0.4,0.6]
+    otsu_method(input)
 
 
 if __name__ == "__main__":
